fe4feeb26f47c4b0b38aa3581f00aa4580c940d2
sboomi <47070729+sboomi@users.noreply.github.com>
2020-05-26 22:18:36 +0200

    The app can now stock the uploaded images in an upload folder

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -19,7 +21,15 @@ soit une probabilité, soit une classification en dur
 """
 Variables globales
 """
+UPLOAD_FOLDER = 'uploads'
 img_flower = []
+img_name = []
+
+"""
+Configuration de l'application
+"""
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 """
 Fonctions de l'application
@@ -27,9 +37,15 @@ Fonctions de l'application
 
 @app.route("/", methods=['GET', 'POST'])
 def show_flower(): 
+    global img_flower, img_name
     if request.method == "POST":
-        img_flower = request.form.get("imgflower") 
-	return render_template("index.html", img_flower=img_flower)
+        img_flower = request.files.getlist("imgflower") 
+        img_name = [secure_filename(img.filename) for img in img_flower ]
+        for i in range(len(img_flower)):
+            img_flower[i].save(os.path.join(app.config['UPLOAD_FOLDER'], img_name[i]))    
+    return render_template("index.html", img_flower=[f'<img src="{UPLOAD_FOLDER}/{name}" alt="{name}">' for name in img_name])
+    
 
+# Active le debug
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
