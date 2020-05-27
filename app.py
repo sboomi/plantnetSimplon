9d1120a2f9b1a33c8ee3e7db5e32c87be52d81ab
sboomi <47070729+sboomi@users.noreply.github.com>
2020-05-27 09:39:36 +0200

    Implemented a dropbox function to stock imgs

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
-from flask import Flask, render_template, request
-from werkzeug.utils import secure_filename
+from flask import Flask, render_template, request, redirect, session, url_for
+from flask_dropzone import Dropzone
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 
 app = Flask(__name__)
+dropzone = Dropzone(app)
 
 """
 BUT DE L'APPLICATION
@@ -16,35 +18,78 @@ stocké dans la variable img_flower
 -Une fois mise en ligne, on applique le modèle de classification
 par la méthode show_flower et on renvoie
 soit une probabilité, soit une classification en dur
+
+Notes: le dropzone provient de ce tutorial
+https://medium.com/@dustindavignon/upload-multiple-images-with-python-flask-and-flask-dropzone-d5b821829b1d
 """
 
-"""
-Variables globales
-"""
-UPLOAD_FOLDER = 'uploads'
-img_flower = []
-img_name = []
 
 """
 Configuration de l'application
 """
+app.config['DROPZONE_UPLOAD_MULTIPLE'] = True
+app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
+app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*'
+app.config['DROPZONE_REDIRECT_VIEW'] = 'results'
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + '/uploads'
+
+app.config['SECRET_KEY'] = 'plantnet'
+
+
+"""
+Variables globales
+"""
+
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+patch_request_class(app) 
+
 
 """
 Fonctions de l'application
 """
 
-@app.route("/", methods=['GET', 'POST'])
-def show_flower(): 
-    global img_flower, img_name
-    if request.method == "POST":
-        img_flower = request.files.getlist("imgflower") 
-        img_name = [secure_filename(img.filename) for img in img_flower ]
-        for i in range(len(img_flower)):
-            img_flower[i].save(os.path.join(app.config['UPLOAD_FOLDER'], img_name[i]))    
-    return render_template("index.html", img_flower=[f'<img src="{UPLOAD_FOLDER}/{name}" alt="{name}">' for name in img_name])
+@app.route('/', methods=['GET', 'POST'])
+def index():
     
+    # set session for image results
+    if "file_urls" not in session:
+        session['file_urls'] = []
+    # list to hold our uploaded image urls
+    file_urls = session['file_urls']
+    # handle image upload from Dropzone
+    if request.method == 'POST':
+        file_obj = request.files
+        for f in file_obj:
+            file = request.files.get(f)
+            
+            # save the file with to our photos folder
+            filename = photos.save(
+                file,
+                name=file.filename    
+            )
+            # append image urls
+            file_urls.append(photos.url(filename))
+            
+        session['file_urls'] = file_urls
+        return "uploading..."
+    # return dropzone template on GET request    
+    return render_template('index.html')
+
+
+@app.route('/results')
+def results():
+    
+    # redirect to home if no images to display
+    if "file_urls" not in session or session['file_urls'] == []:
+        return redirect(url_for('index'))
+        
+    # set the file_urls and remove the session variable
+    file_urls = session['file_urls']
+    session.pop('file_urls', None)
+    
+    return render_template('results.html', file_urls=file_urls)
 
 # Active le debug
 if __name__ == "__main__":
