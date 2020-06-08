b56cd27850043ecfb466639a0d25c0448ad98742
sboomi <47070729+sboomi@users.noreply.github.com>
2020-06-08 11:18:51 +0200

    [Analyzer] added a walkthrough. Need the .pkl file

--- utilities/image_analyzer.py
+++ utilities/image_analyzer.py
@@ -1,4 +1,6 @@
-import modules.grayscaler as gs
+import pickle
+
+import modules.remove_bg as nobg
 import modules.image_loader as il
 #import ..directories
 
@@ -11,18 +13,27 @@ ROOT_DIR = '../'
 UPLOADS_FOLDER = f"{ROOT_DIR}/uploads"
 TEST_FOLDER = f"{ROOT_DIR}/uploads/training"
 
+#A changer en fonction du modèle final
+MODEL = "model.pkl"
+
 def analyze(file_urls):
 
     #Preprocessing
-    ## Grayscaler
-    gs.main(UPLOADS_FOLDER, TEST_FOLDER)
+    ## Remove background
+    nobg.img_process(UPLOADS_FOLDER, TEST_FOLDER)
     X, y, y_names, im_dims =il.prep_data(TEST_FOLDER)
 
     #Classifier
     # Comes with a trained classifier
     #Using the pickle module
+    clf_pkl = open(MODEL,'rb')
+    clf = pickle.load(clf_pkl)
 
     #Prediction
     #Returns something with predictions (list of names, dictionnaries...)
+    y_pred = clf.predict(X)
 
+    #Optional: compute scores
+
+    #return [y_names[i] for i in y_pred]
     return ["Null"] * len(file_urls)
